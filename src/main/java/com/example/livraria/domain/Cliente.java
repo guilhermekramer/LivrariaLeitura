b97adb7d64c8508222db081b56709ddc0dcb4549
Laretz <renatophoenix123@gnail.com>
2023-06-30 11:32:22 -0300

    dto de cliente

--- src/main/java/com/example/livraria/domain/Cliente.java
+++ src/main/java/com/example/livraria/domain/Cliente.java
@@ -1,10 +1,15 @@
 package com.example.livraria.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -19,4 +24,44 @@ public class Cliente extends AbstractEntity{
     Endereco endereco;
     Boolean admin = false;
 
+
+    @Data
+    public static class DtoRequest {
+        @NotBlank(message = "Usuário com nome em branco")
+        String nome;
+        String cpf;
+        @Email (message = "Email")
+        String email;
+        Integer idade;
+        Endereco endereco;
+        Boolean admin = false;
+
+
+        public static Cliente convertToEntity(DtoRequest dto, ModelMapper mapper){
+            return mapper.map(dto, Cliente.class);
+        }
+
+    }
+
+    public static class DtoResponse {
+        String nome;
+        String email;
+        Integer idade;
+
+        public static DtoResponse convertToDto(Cliente c, ModelMapper mapper){
+            return mapper.map(c, DtoResponse.class);
+        }
+
+
+
+        public void generateLinks(Long id){
+            add(linkTo(Cliente.class).slash(id).withSelfRel());  //parei aqui, acho que precisa implementar controller e serviços antes de prosseguir
+            add(linkTo(Cliente.class).withRel("pessoas"));
+            add(linkTo(Cliente.class).slash(id).withRel("delete"));
+        }
+    }
+
+
 }
+
+
